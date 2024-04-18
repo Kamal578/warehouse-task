9ddce7a2928e479782cedaa08d85abf482f03bcd
Kamal E. Ahmadov <AhmadovKEm@kapitalho.az>
2024-04-18 11:26:11 +0400

    configured app module, initialized crud workspace

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: process.env.TYPEORM_SYNC === 'true', // Should not be true in prod 
+      }),
+      inject: [ConfigService],
+    }),
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

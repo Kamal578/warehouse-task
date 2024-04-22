0be74ab7b9c0ed563beecc4ea3a7151ef70c2b48
Kamal Ahmadov <ahmadov.kamal423@gmail.com>
2024-04-22 15:59:25 +0400

    added swagger. /api endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Warehouse API')
+    .setDescription(
+      'Technical task to get acquainted with Docker, Kubernetes, and observability tools.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

dc53223dfaa217f8cb47e91871cdbb6d492c34e0
Kamal Ahmadov <ahmadov.kamal423@gmail.com>
2024-04-22 17:36:43 +0400

    slight changes in log config file

--- winston.config.ts
+++ winston.config.ts
@@ -1,14 +1,13 @@
 import { transports, format } from 'winston';
 import * as path from 'path';
 
-const isDev: boolean = process.env.NODE_ENV === 'development';
+const isProd: boolean = process.env.NODE_ENV === 'production';
 
-const logDirectory = isDev
+const logDirectory = isProd
   ? path.join(__dirname, '..', 'logs')
   : path.join(__dirname, '..', 'devLogs');
 
-// Define the transports based on the environment
-const transportsArray = [
+const transportsArrayDev = [
   new transports.Console(),
   new transports.File({
     filename: path.join(logDirectory, 'info.log'),
@@ -28,16 +27,23 @@ const transportsArray = [
   }),
 ];
 
-if (isDev) {
-  transportsArray.filter(
-    (transport) => transport.level === 'warn' || transport.level === 'error',
-  );
-}
+const transportArrayProd = [
+  new transports.Console(),
+  new transports.File({
+    filename: path.join(logDirectory, 'error.log'),
+    level: 'error',
+  }),
+  new transports.File({
+    filename: path.join(logDirectory, 'warn.log'),
+    level: 'warn',
+  }),
+];
 
 export const winstonConfig = {
-  transports: transportsArray,
+  transports: isProd ? transportArrayProd : transportsArrayDev,
   format: format.combine(
     format.timestamp(),
+    format.cli(),
     format.printf(({ level, message, timestamp }) => {
       return `${timestamp} [${level}] - ${message}`;
     }),

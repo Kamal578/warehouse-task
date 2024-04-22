9e8db076e9a0e5710ae4926f873e55fc5e6cd834
Kamal Ahmadov <ahmadov.kamal423@gmail.com>
2024-04-22 16:08:52 +0400

    modify api endpoint, not available in production

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Warehouse API')
-    .setDescription(
-      'Technical task to get acquainted with Docker, Kubernetes, and observability tools.',
-    )
-    .setVersion('1.0')
-    .build();
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Warehouse API')
+      .setDescription(
+        'Technical task to get acquainted with Docker, Kubernetes, and observability tools.',
+      )
+      .setVersion('1.0')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(3000);
 }

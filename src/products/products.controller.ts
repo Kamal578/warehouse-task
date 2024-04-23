90a906948972c36f74bc76508b88a5a7bb210100
Kamal Ahmadov <ahmadov.kamal423@gmail.com>
2024-04-23 16:05:18 +0400

    added internal server error metric

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,13 +13,22 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { LoggerService } from 'src/logger/logger.service';
+import { Counter } from 'prom-client';
 
 @Controller('products')
 export class ProductsController {
+  private internalServerErrorCount: Counter<string>;
+
   constructor(
     private readonly productsService: ProductsService,
     private readonly logger: LoggerService,
-  ) {}
+  ) {
+    this.internalServerErrorCount = new Counter({
+      name: 'product_internal_server_error_count',
+      help: 'number of internal server errors',
+      labelNames: ['method', 'endpoint'],
+    });
+  }
 
   @Post()
   async create(
@@ -30,6 +39,10 @@ export class ProductsController {
       this.logger.log('Creating a new product');
       return await this.productsService.create(createProductDto);
     } catch (error) {
+      this.internalServerErrorCount.inc({
+        method: 'POST',
+        endpoint: 'products',
+      });
       this.logger.error(
         `Failed to create a new product: ${error.message}`,
         error.stack,
@@ -44,6 +57,10 @@ export class ProductsController {
       this.logger.log('Fetching all products');
       return await this.productsService.findAll();
     } catch (error) {
+      this.internalServerErrorCount.inc({
+        method: 'GET',
+        endpoint: 'products',
+      });
       this.logger.error(
         `Failed to fetch all products: ${error.message}`,
         error.stack,
@@ -58,6 +75,10 @@ export class ProductsController {
       this.logger.log(`Fetching product with ID: ${id}`);
       return await this.productsService.findOne(+id);
     } catch (error) {
+      this.internalServerErrorCount.inc({
+        method: 'GET',
+        endpoint: 'products/:id',
+      });
       this.logger.error(
         `Failed to fetch product with ID ${id}: ${error.message}`,
         error.stack,
@@ -77,6 +98,10 @@ export class ProductsController {
       this.logger.log(`Updating product with ID: ${id}`);
       return await this.productsService.update(+id, updateProductDto);
     } catch (error) {
+      this.internalServerErrorCount.inc({
+        method: 'PUT',
+        endpoint: 'products/:id',
+      });
       this.logger.error(
         `Failed to update product with ID ${id}: ${error.message}`,
         error.stack,
@@ -93,6 +118,10 @@ export class ProductsController {
       this.logger.log(`Deleting product with ID: ${id}`);
       return await this.productsService.remove(+id);
     } catch (error) {
+      this.internalServerErrorCount.inc({
+        method: 'DELETE',
+        endpoint: 'products/:id',
+      });
       this.logger.error(
         `Failed to delete product with ID ${id}: ${error.message}`,
         error.stack,

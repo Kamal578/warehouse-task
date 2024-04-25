2f2d8f6087495fe5b243e3c24849735a93cf826a
Kamal Ahmadov <ahmadov.kamal423@gmail.com>
2024-04-25 12:37:37 +0400

    initial attempt at implementing migrations, probably not working properly

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity'; // Import Product entity
+import { Product } from './entities/product.entity';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { LoggerService } from 'src/logger/logger.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Product]), // Import TypeOrmModule with Product entity
-  ],
+  imports: [TypeOrmModule.forFeature([Product])],
   providers: [ProductsService, LoggerService],
   controllers: [ProductsController],
 })
